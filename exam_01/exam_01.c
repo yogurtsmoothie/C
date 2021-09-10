80ba464a8b785e283e937e755ad765fd932ca3ca
yogurtsmoothie <green7170@naver.com>
2021-09-10 14:47:09 +0900

    first commit

---
+++ exam_01/exam_01.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+	int a, b, c, d;
+	int result;
+
+	printf("첫 번째 계산할 값을 입력하세요 ==>");
+	scanf_s("%d", &a);
+	printf("두 번째 계산할 값을 입력하세요 ==>");
+	scanf_s("%d", &b);
+	printf("세 번째 계산할 값을 입력하세요 ==>");
+	scanf_s("%d", &c);
+	printf("네 번째 계산할 값을 입력하세요 ==>");
+	scanf_s("%d", &d);
+
+	result = a + b + c + d;
+
+	printf("%d + %d + %d + %d = %d \n", a, b, c, d, result);
+}
