04ec118d23849f126130a86d5a500700eb4bd008
yogurtsmoothie <green7170@naver.com>
2021-09-20 23:31:05 +0900

    self study

--- 6_14/6_14.c
+++ 6_14/6_14.c
@@ -6,7 +6,7 @@ int main()
 
 	for (i = 2; i <= 9; i++)
 	{
-		printf("## 제  %d  단 ## \n", i);
+		printf("## 제 %d 단 ## \n", i);
 		for (k = 1; k <= 9; k++)
 		{
 			printf(" %d X %d = %d \n", i, k, i*k);
