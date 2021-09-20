905683b2551c210ab2abd93871f8a2544c912434
yogurtsmoothie <green7170@naver.com>
2021-09-20 23:11:47 +0900

    self study

--- 6_13/6_13.c
+++ 6_13/6_13.c
@@ -4,9 +4,9 @@ int main()
 {
 	int i, k;
 
-	for (i = 0; i < 3; i++)
+	for (i = 2; i <= 9; i++)
 	{
-		for (k = 0; k < 2; k++)
+		for (k = 1; k <= 9; k++)
 		{
 			printf("중첩 for문입니다. (i값: %d, k값: %d)\n", i, k);
 		}
